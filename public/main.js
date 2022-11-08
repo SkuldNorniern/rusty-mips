5a3fb0d76e0cca722f257dc46251a378dc63b5a0
jsonmona <jsonmona@outlook.com>
2022-11-08 16:12:11 +0900

    Remove react template code

--- public/main.js
+++ public/main.js
@@ -5,8 +5,8 @@ const path = require('path');
 function createWindow () {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
