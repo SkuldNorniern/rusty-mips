4167e30c6644d0991c6b637d2e667441482c2785
SkuldNorniern <skuldnorniern@gmail.com>
2022-12-14 14:42:44 +0000

    Fix forward

--- native/src/executor/pipeline/stage/forward.rs
+++ native/src/executor/pipeline/stage/forward.rs
@@ -9,25 +9,25 @@ pub fn fwd_ctrl(
 ) -> forward_unit::FwdUnit {
     let mut fwd_unit = _fwd_unit;
 
-    if _mem_wb.ctr_unit.mem_to_reg == 1
+    if _mem_wb.ctr_unit.reg_write == 1
         && _mem_wb.rd != 0
         && _mem_wb.rd == _id_ex.rs
         && (_ex_mem.rd != _id_ex.rs || _ex_mem.ctr_unit.reg_write == 0)
     {
         fwd_unit.fwd_a = 1;
-    } else if _mem_wb.ctr_unit.mem_to_reg == 1 && _mem_wb.rd != 0 && _mem_wb.rd == _id_ex.rs {
+    } else if _ex_mem.ctr_unit.reg_write == 1 && _ex_mem.rd != 0 && _ex_mem.rd == _id_ex.rs {
         fwd_unit.fwd_a = 2;
     } else {
         fwd_unit.fwd_a = 0;
     }
 
-    if _mem_wb.ctr_unit.mem_to_reg == 1
+    if _mem_wb.ctr_unit.reg_write == 1
         && _mem_wb.rd != 0
         && _mem_wb.rd == _id_ex.rt
         && (_ex_mem.rd != _id_ex.rt || _ex_mem.ctr_unit.reg_write == 0)
     {
         fwd_unit.fwd_b = 1;
-    } else if _mem_wb.ctr_unit.mem_to_reg == 1 && _mem_wb.rd != 0 && _mem_wb.rd == _id_ex.rt {
+    } else if _ex_mem.ctr_unit.reg_write == 1 && _ex_mem.rd != 0 && _ex_mem.rd == _id_ex.rt {
         fwd_unit.fwd_b = 2;
     } else {
         fwd_unit.fwd_b = 0;
